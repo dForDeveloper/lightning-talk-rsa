41005fb6aa75f200007853f73b07d089fa5d3fc7
Jeo D <e8gea6w5z8id@opayq.com>
2018-12-14 08:31:49 -0700

    Add back button feature

--- js/slides.js
+++ js/slides.js
@@ -9,7 +9,20 @@ document.querySelectorAll('.button--next').forEach(button => {
 document.querySelectorAll('.button--back').forEach(button => {
   button.addEventListener('click', function(event) {
     event.preventDefault();
-    document.querySelector('.present').classList.replace('present', 'future');
-    document.querySelector('.past').classList.replace('past', 'present');
-  })
-})
+    document.querySelector('.present').classList.replace('present', 'future')
+    document.querySelectorAll('.past').forEach((slide, index, slides) => {
+      if (index === slides.length - 1) {
+        slide.classList.replace('past', 'present');
+      }
+    });
+  });
+});
+
+document.querySelector('.button--app-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.querySelector('.present').classList.replace('present', 'past');
+  document.querySelectorAll('.future').forEach(slide => {
+    slide.classList.replace('future', 'past');
+  });
+  document.querySelector('.slide__app').classList.replace('past', 'present');
+});
