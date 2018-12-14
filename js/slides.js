b6e1e391f32aa6f03dec20be23ce1319ffa5aebc
Jeo D <e8gea6w5z8id@opayq.com>
2018-12-14 08:11:08 -0700

    Add images of Rivest, Shamir, and Adleman

--- js/slides.js
+++ js/slides.js
@@ -1,8 +1,15 @@
 document.querySelectorAll('.button--next').forEach(button => {
   button.addEventListener('click', function(event) {
     event.preventDefault();
-    console.log('click')
     document.querySelector('.present').classList.replace('present', 'past');
     document.querySelector('.future').classList.replace('future', 'present');
+  });
+});
+
+document.querySelectorAll('.button--back').forEach(button => {
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('.present').classList.replace('present', 'future');
+    document.querySelector('.past').classList.replace('past', 'present');
   })
 })
